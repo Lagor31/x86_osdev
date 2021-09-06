b56ab2e3dd9383d7acd71b55fb08677b2c040eb0
Lagor <federicogorla@gmail.com>
2021-09-06 19:48:38 +0200

    Added notes

--- kernel/signals.c
+++ kernel/signals.c
@@ -8,7 +8,7 @@ void init_signals(Signals *sigs) {
 }
 
 u32 sys_kill(u32 pid, u32 signal) {
-  //kprintf("Delivering signal %d to pid %d\n", signal, pid);
+  // kprintf("Delivering signal %d to pid %d\n", signal, pid);
   Thread *t = get_thread(pid);
   if (t != NULL) {
     t->signals.pending |= (t->signals.active & signal);
@@ -19,19 +19,17 @@ u32 sys_kill(u32 pid, u32 signal) {
 
 const char *get_sig_name(u32 sig_num) {
   if (sig_num == SIGKILL) return "SIGKILL";
-
   return "UNKSIG";
 }
 
 bool handle_signals(Thread *t) {
-  bool can_continue = TRUE;
   if (t == NULL) return TRUE;
   Signals *sigs = &t->signals;
   if (sigs->pending == 0) return TRUE;
   i32 i = 1;
   for (i = 1; i <= SIG_NUM; i++) {
     if ((sigs->pending & sigs->active) & 1 << i) {
-      //kprintf("Handling sig %s for pid %d\n", get_sig_name(1 << i), t->pid);
+      kprintf("Received sig %s pid %d\n", get_sig_name(1 << i), t->pid);
       sigs->pending &= ~(1 << i);
       // Kill thread
       if (1 << i == SIGKILL) {
